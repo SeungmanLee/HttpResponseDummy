c7a16811eb154314d99e451645f19fc6bf1a90c0
Seungman Lee <kyww1119@gmail.com>
2016-05-22 16:49:54 +0900

    add sources

---
+++ src/main/java/org/smlee/HttpResponseDummyApplication.java
@@ -0,0 +1,12 @@
+package org.smlee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpResponseDummyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpResponseDummyApplication.class, args);
+	}
+}
---
+++ src/test/java/org/smlee/HttpResponseDummyApplicationTests.java
@@ -0,0 +1,16 @@
+package org.smlee;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = HttpResponseDummyApplication.class)
+public class HttpResponseDummyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
